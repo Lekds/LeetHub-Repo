80b133bf0a3dd7a23e6d1b7c35690d1f5fe37ffa
Lekds <91030525+Lekds@users.noreply.github.com>
2022-06-14 21:32:35 +0100

    Time: 66 ms (56.02%), Space: 14 MB (29.66%) - LeetHub

---
+++ 13-roman-to-integer/13-roman-to-integer.py
@@ -0,0 +1,14 @@
+class Solution:
+    def romanToInt(self, s):
+        romandic = {'I': 1, 'V': 5, 'X': 10, 'L' : 50, 'C' : 100, 'D' : 500, 'M' : 1000}   
+        result = 0
+        for i in range(len(s)):
+            if i + 1 == len(s) or romandic[s[i]] >= romandic[s[i + 1]] :
+                result = result + romandic[s[i]]
+            else:
+                result = result - romandic[s[i]]
+        return result
+Task = Solution()
+print(Task.romanToInt("VIII"))
+print(Task.romanToInt("LVIII"))
+print(Task.romanToInt("MCMXCIV"))
